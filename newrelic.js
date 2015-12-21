287c82fe5d4324b4fa104c1930fd2148918d99a2
gaskar <gaskar18@gmail.com>
2015-12-21 22:58:39 +0400

    newrelic config

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['fanscount'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '868437a4ad0dcc0cb504bb103ddae9d4ba2e9e8e',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
