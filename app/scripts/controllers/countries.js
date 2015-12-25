b631601ccfd86c6f94a3d9d8f0933abd101e3a21
gaskar <gaskar18@gmail.com>
2015-12-26 01:36:12 +0400

    show flags per rows, refactoring

--- app/scripts/controllers/countries.js
+++ app/scripts/controllers/countries.js
@@ -5,25 +5,51 @@ import Template from '../../libs/mustache.min.js';
 import countryService from '../services/countries';
 
 const props = {
-  template: `<tr>
+  template: `
+            {{#rows}}
+              <tr>
+                {{#countries}}
                 <td>
                     <span>{{name}}</span>
                     <img src="{{flag}}" width="100" />
                 </td>
-             </tr>`,
-  className: '',
-  containerId: '#countries'
+                {{/countries}}
+             </tr>
+             {{/rows}}`,
+  containerId: '#countries',
+  itemsPerRow: 5
 };
+
+/*
+  output example {rows: [countries: [], countries[]]}
+*/
+let createCountriesList = function(countries) {
+  let countriesArray = [];
+  const itemsPerRow = props.itemsPerRow;
+
+  for(let i = 0; i < countries.length; i++) {
+    let position = parseInt(i / itemsPerRow);
+
+    if(i % itemsPerRow === 0) {
+      countriesArray[position] = {
+        countries: []
+      };
+    }
+    
+    countriesArray[position].countries.push(countries[i]);
+  }
+
+  return countriesArray;
+}
 
 exports.init = function() {
   var el = $(props.containerId);
   // console.log()
   return countryService.getAll()
     .then(function(countries) {
-        countries.forEach(function(country) {
-          let countryEl = Template.render(props.template, country);
+      const countriesArray = createCountriesList(countries);
 
-          el.append(countryEl);
-      });
+      el.html(Template.render(props.template, {rows: countriesArray}));
+
     });
 };
