044ac8fd6f1348f7009480a31a150188681d9ee1
gaskar <gaskar18@gmail.com>
2016-02-25 15:57:39 +0400

    changes in scraper

--- scrape/dbScripts.js
+++ scrape/dbScripts.js
@@ -13,73 +13,73 @@ let mongoose = require('mongoose-q')(require('../node_modules/mongoose')),
 
 mongoose.connect('mongodb://localhost/fanscount');
 
-exports. addToDb = function(clubInfo) {
-  let country;
-  getCountry(clubInfo.country)
-    .then(function (countryObj) {
-      country = countryObj;
-      return getLeague(clubInfo.league, country);
-    })
-    .then(function (league) {
-      clubInfo.league = {
-        leagueName: league.name,
-        leagueId: league._id
-      };
+exports.addToDb = function (clubInfo) {
+    let country;
+    getCountry(clubInfo.country)
+        .then(function (countryObj) {
+            country = countryObj;
+            return getLeague(clubInfo.league, country);
+        })
+        .then(function (league) {
+            clubInfo.league = {
+                leagueName: league.name,
+                leagueId: league._id
+            };
 
-      clubInfo.country = {
-        countryName: country.name,
-        countryId: country._id
-      };
-      createdInLastWave++;
+            clubInfo.country = {
+                countryName: country.name,
+                countryId: country._id
+            };
+            createdInLastWave++;
 
-      console.log(clubInfo.teamNumber, createdInLastWave);
+            console.log(clubInfo.teamNumber, createdInLastWave);
 
-      Club.create(clubInfo);
-    })
-    .catch(function(err) {
-      console.log('error on creating', teamNumber);
-    });
-}
+            Club.create(clubInfo);
+        })
+        .catch(function (err) {
+            console.log('error on creating', teamNumber);
+        });
+};
 
 
 var getLeague = function (leagueName, country) {
     for (let i = 0; i < leagues.length; i++) {
-      if (leagues[i].name === leagueName && leagues[i].country.countryName === country.name) {
-        return Q(leagues[i]);
-      }
+        if (leagues[i].name === leagueName && leagues[i].country.countryName === country.name) {
+            return Q(leagues[i]);
+        }
     }
 
     let league = new League({
-      name: leagueName,
-      country: {
-        countryName: country.name,
-        countryId: country._id
-      }
+        name: leagueName,
+        country: {
+            countryName: country.name,
+            countryId: country._id
+        }
     });
 
     return league.saveQ()
-            .then(function (league) {
-              leagues.push(league);
+        .then(function (league) {
+            leagues.push(league);
 
-              return league;
-            });
+            return league;
+        });
 };
 
 var getCountry = function (countryName) {
-  for (let i = 0; i < countries.length; i++) {
-    if (countries[i].name === countryName) {
-      return Q(countries[i]);
+    for (let i = 0; i < countries.length; i++) {
+        if (countries[i].name === countryName) {
+            return Q(countries[i]);
+        }
     }
-  }
 
-  let country = new Country({
-    name: countryName
-  });
+    let country = new Country({
+        name: countryName
+    });
 
-  return country.saveQ()
-          .then(function (country) {
+    return country.saveQ()
+        .then(function (country) {
             countries.push(country);
 
             return country;
-          });
+        });
 };
